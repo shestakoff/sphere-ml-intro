dd6f9439981f2cea430f64691521fe9334d88212
Aleksey Spasenov <a.spasenov@corp.mail.ru>
2020-10-06 20:19:41 +0300

    lec2: Add examples

---
+++ 2020/lecture02-tasks/classification/helpers.py
@@ -0,0 +1,17 @@
+import sklearn.metrics as metrics
+import matplotlib.pyplot as plt
+
+def plot_roc_curve(y_true, y_pred):
+    fpr, tpr, threshold = metrics.roc_curve(y_true, y_pred)
+    roc_auc = metrics.auc(fpr, tpr)
+
+    plt.title('Receiver Operating Characteristic')
+    plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
+    plt.legend(loc = 'lower right')
+    plt.plot([0, 1], [0, 1],'r--')
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.grid()
+    plt.show()
